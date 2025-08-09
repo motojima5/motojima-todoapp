ff504dafbfac7168ec0a16a9498ec3873037e3a9
Motojima <r.motojima5@gmail.com>
2025-08-09 18:37:16 +0900

    destroy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -38,7 +38,7 @@ class BoardsController < ApplicationController
   end
 
   def destroy
-    board = Boards.find(params[:id])
+    board = current_user.boards.find(params[:id])
     board.destroy!
     redirect_to root_path, status: :see_other, notice: '削除に成功しました'
   end

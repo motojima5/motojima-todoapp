9debc47b92ce1e1d521f1cbba52b2aa578e5c5a9
Motojima <r.motojima5@gmail.com>
2025-08-09 11:52:36 +0900

    rubocopのエラー修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -11,9 +11,9 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
-      redirect_to board_path(@board), notice: '保存できたよ'
+      redirect_to board_path(@board), notice: "保存できたよ"
     else
-      flash.now[:error] = '保存に失敗しました'
+      flash.now[:error] = "保存に失敗しました"
       render :new, status: :unprocessable_entity
     end
   end

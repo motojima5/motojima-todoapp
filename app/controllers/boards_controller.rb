2c22ad8619d551a205e4fdb87f085b0e64218745
Motojima <r.motojima5@gmail.com>
2025-08-09 09:34:39 +0900

    new 画面の作成 & モデル名に合わせた名称変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,4 @@
-class TodosController < ApplicationController
+class BoardsController < ApplicationController
   before_action :authenticate_user!, only: [ :new, :create, :edit, :update, :destroy ]
 
   def index
